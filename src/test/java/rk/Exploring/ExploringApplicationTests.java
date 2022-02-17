5356aa76edfa50f0d91bfbf011a30cbafb9d252c
RahimaKhanBM <rahima.khan@benchmatrix.ca>
2022-02-17 11:11:10 +0500

    Initial commit

---
+++ src/test/java/rk/Exploring/ExploringApplicationTests.java
@@ -0,0 +1,13 @@
+package rk.Exploring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
